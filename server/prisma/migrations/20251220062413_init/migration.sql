47ee3df1d6647de6363a709f5a28122a22b0d083
HK Patel <hkssoa@gmail.com>
2025-12-20 11:57:48 +0530

    fix small error

---
+++ server/prisma/migrations/20251220062413_init/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NULL,
+    `googleId` VARCHAR(191) NULL,
+    `codeforces_handle` VARCHAR(191) NULL,
+    `profilePicture` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_googleId_key`(`googleId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Bookmark` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `problem_id` VARCHAR(191) NOT NULL,
+    `problem_name` VARCHAR(191) NOT NULL,
+    `rating` INTEGER NULL,
+    `tags` JSON NULL,
+    `userId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Bookmark` ADD CONSTRAINT `Bookmark_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
